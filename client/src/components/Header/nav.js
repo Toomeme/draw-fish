b77121f10768bce2741ae8880ca829b50123b85d
Jake Toomey <jtoom238@gmail.com>
2022-03-09 14:58:28 -0500

    scaling fixes

--- client/src/components/Header/nav.js
+++ client/src/components/Header/nav.js
@@ -11,19 +11,19 @@ const logout = event => {
 <Menu>
           {Auth.loggedIn() ? (
             <>
-              <Link to="/profile" className="menu-item">Me</Link>
+              <Link to="/profile" className="menu-item text-light">Me</Link>
               <br></br>
               <br></br>
-              <a href="/" onClick={logout} className="menu-item">
+              <a href="/" onClick={logout} className="menu-item text-light">
                 Logout
               </a>
             </>
           ) : (
             <>
-              <Link to="/login" className="menu-item">Login</Link>
+              <Link to="/login" className="menu-item text-light">Login</Link>
               <br></br>
               <br></br>
-              <Link to="/signup" className="menu-item">Signup</Link>
+              <Link to="/signup" className="menu-item text-light">Signup</Link>
             </>
           )}
         </Menu>
