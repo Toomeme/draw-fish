e6ce066e4e3bc39231f6d176d71b6e990adc2748
Jacob Toomey <jtoom238@gmail.com>
2024-02-15 16:57:10 -0500

    Update index.js

--- client/src/components/Board/index.js
+++ client/src/components/Board/index.js
@@ -1,99 +1,65 @@
 import DrawableCanvas from "../Canvas";
-import { FaSave,FaUndo,FaEraser } from "react-icons/fa";
+import { FaSave, FaUndo, FaEraser } from "react-icons/fa";
 import UserList from "./UserList";
 import Auth from '../../utils/auth';
-import React from "react";
-import {SocketContext} from './context/socket';
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { SocketContext } from './context/socket';
 import { Redirect } from 'react-router-dom';
-const { Component } = require("react");
 
+const Drawing = (props) => {
+  const [drawing, setDrawing] = useState(false);
+  const [current, setCurrent] = useState({ x: 0, y: 0 });
+  const [fileID, setFileID] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [canvas, setCanvas] = useState(null);
+  const [context, setContext] = useState(null);
+  const socket = useContext(SocketContext);
+  const canvasRef = useRef(null);
 
-const loggedIn = Auth.loggedIn();
-var canvas, context;
+  const loggedIn = Auth.loggedIn();
 
-var fileID = '';
+  useEffect(() => {
+    if (canvasRef.current) {
+      setCanvas(canvasRef.current.children[0].children[1]);
+      setContext(canvasRef.current.children[0].children[1].getContext("2d"));
+    }
+  }, [canvasRef]);
 
-/* VARIABLES */
-var drawing = false;
-// determines the current x or y position
-var current = {x: 0, y: 0};
-
-
-export class Drawing extends Component {
-  // this.context refers to socket.io making it available for all components
-  static contextType = SocketContext;
-
-  constructor(props) {
-   super(props);
-    this.state = {
-      canvasWidth: 1000,
-      canvasHeight: 500,
-      brushColor: "#444",
-      catenaryColor: "#0a0302",
-      lazyRadius: 12,
-      brushRadius: 10,
-      clear: false,
-      hideInterface: false,
-      onChange: null,
-      loadTimeOffset: 5,
-      backgroundColor: "#FFF",
-      hideGrid: false,
-      disabled: false,
-      saveData: "",
-      immediateLoading: true,
-      usingType: "draw",
-      username: null,
-      room: null,
-      userList: [],
-      submitted:false,
-      redirect:false
-    };
-  }
-  
-
-
-
-  componentDidMount() {
-    canvas = document.getElementsByName("canvas")[0].children[0].children[1];
-    context = canvas.getContext("2d");
-    this.whiteboard = React.createRef();
-    this.context.on('drawing', data => this.onDrawingEvent(data))
-      
-    this.context.emit("join", {
-      username: this.props.username,
-      room: this.props.room
-    });
-
-    this.context.on("saved", saved =>{
-      fileID = saved;
-      this.setState({submitted:false,redirect:true});
-    console.log(fileID)
-  });
-  
-    this.context.on("joined", joined => {
-      this.setState({
-        id: joined.id,
-        username: joined.username,
-        room: joined.room
+  useEffect(() => {
+    if (context) {
+      socket.on('drawing', data => onDrawingEvent(data));
+      socket.emit("join", {
+        username: props.username,
+        room: props.room
       });
-      var canvasstate = this.saveableCanvas.getSaveData()
-      this.context.emit("joinstate",{canvasstate})
-    });
 
-    this.context.on("joinstate",canvas =>{
-      this.saveableCanvas.loadSaveData(canvas)});
-  
-    this.context.on("users", users => {
-      this.setState({
-        userList: users
+      socket.on("saved", saved => {
+        setFileID(saved);
+        setSubmitted(false);
+        setRedirect(true);
       });
-    });
-  }
 
-  
+      socket.on("joined", joined => {
+        setSubmitted(false);
+        setRedirect(false);
+        setRoom(joined.room);
+        var canvasstate = saveableCanvas.getSaveData();
+        socket.emit("joinstate", { canvasstate });
+      });
 
-   onDrawingEvent(data) {
-    this.drawLine(
+      socket.on("joinstate", canvas => {
+        saveableCanvas.loadSaveData(canvas)
+      });
+
+      socket.on("users", users => {
+        setUserList(users);
+      });
+    }
+  }, [context, props.username, props.room, socket]);
+
+  const onDrawingEvent = (data) => {
+    drawLine(
       data.x0,
       data.y0,
       data.x1,
@@ -103,17 +69,14 @@ export class Drawing extends Component {
       false,
       true,
     );
-  }
-  
-  drawLine(x0, y0, x1, y1, color,brushRadius, emit,duplicate) {
+  };
+
+  const drawLine = (x0, y0, x1, y1, color, brushRadius, emit, duplicate) => {
     // hard code offset since canvas size is fixed.
     var canvasTopPosition = 125;
     var canvasLeftPosition = 97;
-    console.log(canvasTopPosition+"TOP");
-    console.log(canvasLeftPosition+"LEFT");
-    // If Duplicate is false, it will draw on both monitors and cause duplication.
-    if(duplicate)
-    {
+
+    if (duplicate) {
       context.beginPath();
       context.moveTo(x0, y0);
       context.lineTo(x1, y1);
@@ -123,170 +86,176 @@ export class Drawing extends Component {
       context.stroke();
       context.closePath();
     }
-      if (!emit) {
-        return;
-      }
-      this.context.emit('drawing', {
-        x0: x0 - canvasLeftPosition,
-        y0: y0 - canvasTopPosition,
-        x1: x1 - canvasLeftPosition,
-        y1: y1 - canvasTopPosition,
-        color: color,
-        brushRadius : this.state.brushRadius,
-        room: this.state.room,
-        type: this.state.usingType,
-        globalCompositeOperation: context.globalCompositeOperation,
-        message_type: "draw"
-      });   
-  }
-  onMouseDown = (e) => {
-    drawing = true;
-    current.x = e.clientX || e.touches[0].clientX;
-    current.y = e.clientY || e.touches[0].clientY;
+    if (!emit) {
+      return;
+    }
+    socket.emit('drawing', {
+      x0: x0 - canvasLeftPosition,
+      y0: y0 - canvasTopPosition,
+      x1: x1 - canvasLeftPosition,
+      y1: y1 - canvasTopPosition,
+      color: color,
+      brushRadius: brushRadius,
+      room: props.room,
+      type: state.usingType,
+      globalCompositeOperation: context.globalCompositeOperation,
+      message_type: "draw"
+    });
   };
 
-  onMouseUp = (e) => {
+  const onMouseDown = (e) => {
+    setDrawing(true);
+    setCurrent({ x: e.clientX || e.touches[0].clientX, y: e.clientY || e.touches[0].clientY });
+  };
+
+  const onMouseUp = (e) => {
     if (!drawing) {
       return;
     }
-    drawing = false;
-    this.drawLine(
+    setDrawing(false);
+    drawLine(
       current.x,
       current.y,
       e.clientX,
       e.clientY,
-      this.state.brushColor,
-      this.state.brushRadius,
+      state.brushColor,
+      state.brushRadius,
       true,
       false,
     );
   };
-  onMouseMove = (e) => {
+
+  const onMouseMove = (e) => {
     if (!drawing) {
       return;
     }
-      this.drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY,this.state.brushColor,this.state.brushRadius,true,false);      
-      current.x = e.clientX || e.touches[0].clientX;
-      current.y = e.clientY || e.touches[0].clientY;
+    drawLine(
+      current.x,
+      current.y,
+      e.clientX || e.touches[0].clientX,
+      e.clientY || e.touches[0].clientY,
+      state.brushColor,
+      state.brushRadius,
+      true,
+      false,
+    );
+    setCurrent({ x: e.clientX || e.touches[0].clientX, y: e.clientY || e.touches[0].clientY });
   };
 
-
-  render() {
-    if (this.state.redirect){
-      return <Redirect push to={'/submit/'+ this.state.room + fileID}/>;
-    }
-    if (this.state.submitted){
-      return <div className="justify-center">Loading...</div>
-    }
-
-    return (
-
-        <main>
-      
-        <div
-          name="canvas"
-          onMouseDown={this.onMouseDown}
-          onMouseUp={this.onMouseUp}
-          onMouseMove={this.onMouseMove}
-          onMouseLeave={() => drawing = false} // Determines if the mouse is outside the div
-        >
-          <DrawableCanvas ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
-          brushColor={this.state.brushColor}
-          brushRadius={this.state.brushRadius}
-          lazyRadius={this.state.lazyRadius}
-          canvasWidth={this.state.canvasWidth}
-          canvasHeight={this.state.canvasHeight}
-          />
-          </div>
-          <div className='tools ml-3'>
-          <button
-          data-aos="fade-left"
-          className="btn col-auto col-md-3"
-            onClick={() => {
-              localStorage.setItem(
-                "savedDrawing",
-                this.saveableCanvas.getSaveData()
-              );
-            }}
-          >
-            <FaSave />
-          </button>
-          <button
-          data-aos="fade-left"
-          className="btn col-auto col-md-3"
-            onClick={() => {
-              this.saveableCanvas.eraseAll();
-            }}
-          >
-            <FaEraser />
-          </button>
-          <button
-          data-aos="fade-left"
-          className="btn col-auto col-md-3"
-            onClick={() => {
-              this.saveableCanvas.undo();
-            }}
-          >
-            <FaUndo />
-          </button>
-          <div data-aos="fade-left">
-          <label>Color:</label>
-            <input
-              type="color"
-              value={this.state.brushColor}
-              onChange={e =>
-                this.setState({ brushColor: e.target.value })
-              }
-            />
-          </div>
-
-          <div data-aos="fade-left">
-            <label>Radius:</label>
-            <input
-              type="number"
-              value={this.state.brushRadius}
-              onChange={e =>
-                this.setState({ brushRadius: parseInt(e.target.value, 10) })
-              }
-            />
-          </div>
-          <div data-aos="fade-left">
-            <label>Stabilization:</label>
-            <input
-              type="number"
-              value={this.state.lazyRadius}
-              onChange={e =>
-                this.setState({ lazyRadius: parseInt(e.target.value, 10) })
-              }
-            />       
-            <button
-            className="btn col-12 col-md-3"
-            onClick={ () => {
-              if (loggedIn){
-              var image = this.saveableCanvas.getDataURL();
-              this.context.emit('image', {image:image,
-                room: this.state.room, message_type: "image"});
-              this.setState({submitted: true})
-              }
-            }}
-          >
-          Publish!
-          </button>
-          </div>
-
-        </div>
-          <div className="user-list" data-aos="fade-right">
-          <h3>Room:</h3>
-          <h5>{this.state.room}</h5>
-          <br></br>
-          <h3>Users:</h3>
-           <UserList userList={this.state.userList} />
-          </div>
-        
-        </main>
-        
-    );
+  if (redirect) {
+    return <Redirect push to={'/submit/' + props.room + fileID} />;
   }
-}
+  if (submitted) {
+    return <div className="justify-center">Loading...</div>;
+  }
+
+  return (
+    <main>
+      <div
+        name="canvas"
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
+        onMouseLeave={() => setDrawing(false)}
+        ref={canvasRef}
+      >
+        <DrawableCanvas
+          ref={canvasDraw => (saveableCanvas = canvasDraw)}
+          brushColor={state.brushColor}
+          brushRadius={state.brushRadius}
+          lazyRadius={state.lazyRadius}
+          canvasWidth={state.canvasWidth}
+          canvasHeight={state.canvasHeight}
+        />
+      </div>
+      <div className='tools ml-3'>
+        <button
+          data-aos="fade-left"
+          className="btn col-auto col-md-3"
+          onClick={() => {
+            localStorage.setItem(
+              "savedDrawing",
+              saveableCanvas.getSaveData()
+            );
+          }}
+        >
+          <FaSave />
+        </button>
+        <button
+          data-aos="fade-left"
+          className="btn col-auto col-md-3"
+          onClick={() => {
+            saveableCanvas.eraseAll();
+          }}
+        >
+          <FaEraser />
+        </button>
+        <button
+          data-aos="fade-left"
+          className="btn col-auto col-md-3"
+          onClick={() => {
+            saveableCanvas.undo();
+          }}
+        >
+          <FaUndo />
+        </button>
+        <div data-aos="fade-left">
+          <label>Color:</label>
+          <input
+            type="color"
+            value={state.brushColor}
+            onChange={e =>
+              setState({ ...state, brushColor: e.target.value })
+            }
+          />
+        </div>
+
+        <div data-aos="fade-left">
+          <label>Radius:</label>
+          <input
+            type="number"
+            value={state.brushRadius}
+            onChange={e =>
+              setState({ ...state, brushRadius: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <div data-aos="fade-left">
+          <label>Stabilization:</label>
+          <input
+            type="number"
+            value={state.lazyRadius}
+            onChange={e =>
+              setState({ ...state, lazyRadius: parseInt(e.target.value, 10) })
+            }
+          />
+          <button
+            className="btn col-12 col-md-3"
+            onClick={() => {
+              if (loggedIn) {
+                var image = saveableCanvas.getDataURL();
+                socket.emit('image', {
+                  image: image,
+                  room: state.room,
+                  message_type: "image"
+                });
+                setSubmitted(true);
+              }
+            }}
+          >
+            Publish!
+          </button>
+        </div>
+      </div>
+      <div className="user-list" data-aos="fade-right">
+        <h3>Room:</h3>
+        <h5>{state.room}</h5>
+        <br></br>
+        <h3>Users:</h3>
+        <UserList userList={state.userList} />
+      </div>
+    </main>
+  );
+};
 
 export default Drawing;
